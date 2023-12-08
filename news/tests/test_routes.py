3d8a01d027cd8e2843a408e5e7c5b25759aa8012
kireich77 <kireich@gmail.com>
2023-12-08 16:54:44 +0300

    выходной

---
+++ news/tests/test_routes.py
@@ -0,0 +1,28 @@
+# news/tests/test_routes.py
+from http import HTTPStatus
+
+from django.test import TestCase
+from django.urls import reverse
+
+from news.models import News
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK) 
